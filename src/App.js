15690bb347665d9a0e4a05ae299dc1b7a2a541fd
zedonora <timeundtime@gmail.com>
2019-02-16 17:51:26 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "김영관",
+    birthday: "891207",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "김일관",
+    birthday: "891207",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "김이관",
+    birthday: "891207",
+    gender: "남자",
+    job: "대학생"
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
       </div>
     );
   }
